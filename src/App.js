19ca3bd3071ff64c144ced6e0a8f5353a7358393
Dayane <daydantas1028@gmail.com>
2022-02-23 17:17:06 -0300

    Footer and Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
-    <div className="App">
       <Router>
-        <ul>
-          <Link to='./'>Home</Link>
-          <Link to='./contact'>Contato</Link>
-          <Link to='./company'>Empresa</Link>
-          <Link to='./newproject'>Novo Projeto</Link>
-        </ul>
+        <Navbar/>
       <Switch>
         <Container customClass='min-height'>
         <Route exact path="/">
           <Home/>
+        </Route>
+        <Route path="/projects">
+          <Projects/>
         </Route>
         <Route path="/company">
           <Company/>
@@ -31,8 +31,8 @@ function App() {
         </Route>
         </Container>
       </Switch>
+      <Footer/>
       </Router>
-    </div>
   );
 }
 
