c7bc600ba012b7753a44198bb19e7ebf57dc997a
Dayane <daydantas1028@gmail.com>
2022-02-25 17:08:32 -0300

    form componization

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,17 @@
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+function ProjectForm({btntext}){
+    return(
+        <form className={styles.form}>
+            <Input type='text' text='Nome do projeto' name='name' placeholder='Insira o Nome do projeto'/>
+            <Input type='number' text='Orçamento do projeto' name='budgt' placeholder='Insira o orçamento total' />        
+            <Select name='category_id' text='Tipo do projeto' /> 
+            <SubmitButton text={btntext} />
+        </form>
+    )
+}
+
+export default ProjectForm
